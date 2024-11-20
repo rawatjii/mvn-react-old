c6952165c9954c739a2fb8ad37115b6cdc529ca1
awaneeshgtf <awneesh@gtftechnologies.com>
2024-11-20 12:45:45 +0530

    video paused in bangalore

--- src/frontend/pages/micro/mvn-aeroone-bangalore/Index.jsx
+++ src/frontend/pages/micro/mvn-aeroone-bangalore/Index.jsx
@@ -18,22 +18,22 @@ const data = {
   video1: {
     title: "EXPERIENCE THE BLISS OF ENDLESS VISTAS",
     // desc:'Our 13.5ft High Ceilings Will Set A Higher Standard Of Space Utilisation In Harmony With Design Aesthetics & Sophistication.',
-    isVdo: true,
+    isVdo: false,
   },
   video2: {
     title: "ESCAPE TO EXCLUSIVITY",
     desc: "Dive into your own private pool with a grand view, just a few steps away from your living room",
-    isVdo: true,
+    isVdo: false,
   },
   video3: {
     title: "ELEVATED LIVING",
     desc: "OUR 13.5FT HIGH CEILINGS WILL SET A HIGHER STANDARD OF SPACE UTILISATION IN HARMONY WITH DESIGN AESTHETICS & SOPHISTICATION",
-    isVdo: true,
+    isVdo: false,
   },
   masterBedroom: {
     title: "MASTER BEDROOM",
     desc: "If You Think You Know What Lavishness Means It's Time You Got A New Perspective",
-    isVdo: true,
+    isVdo: false,
   },
   Slides: {
     isshow: true,
