507592b37a3c22676385238a2eb9b1e8513d32bb
Arman Khan <arman.khan@iqsetters.in>
2024-11-28 18:35:47 +0530

    changes in contact modal

--- src/frontend/components/MicroPage/FloorPlan.jsx
+++ src/frontend/components/MicroPage/FloorPlan.jsx
@@ -194,7 +194,7 @@ const MicroFloorPlan = ({ data }) => {
         )}
       </Container>
 
-      <CustomModal show={isShowModal} hide={isHideModal} />
+      <CustomModal show={isShowModal} hide={isHideModal} projectName={'MVN-AeroOne-Gurgaon'}/>
 
       <Lightbox
         open={index >= 0}
