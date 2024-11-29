c7fe4d3ce530f36d5dd150bbd733a9f73704e0d4
skgtf <Savan.kumar@gtftechnologies.com>
2024-11-29 18:59:31 +0530

    updated by savan

--- src/frontend/components/Card.jsx
+++ src/frontend/components/Card.jsx
@@ -1,32 +1,33 @@
 import React from "react";
-import diamondIMG from '../assets/images/icons/diamond.png';
+import diamondIMG from "../assets/images/icons/diamond.png";
 
-const CustomCard = ({className, title, location, extra, desc, extraTxt})=>{
-  
-  return(
+const CustomCard = ({ className, title, location, extra, desc, extraTxt }) => {
+  return (
     <div className={`overview_card ${className}`}>
       <div className="diamond_img_strip">
         <img src={diamondIMG} className="img-fluid" alt="image" />
       </div>
       {title && (
         <div className="title">
-        <h1 className="pr_name">{title}</h1>
-        <h6 className="location">{location}</h6>
-      </div>
+          <h1 className="pr_name">{title}</h1>
+          <h6 className="location">{location}</h6>
+        </div>
       )}
-      
 
       {extra && <p className="extra">{extra}</p>}
 
-      {
-        desc && Array.isArray(desc) ?  desc.map((el, i)=>(
-          <p className="desc" key={`desc-${i}`}>{el}</p>  
-        )):  <p  className="desc">{desc}</p>
-      }
+      {desc && Array.isArray(desc) ? (
+        desc.map((el, i) => (
+          <p className="desc" key={`desc-${i}`}>
+            {el}
+          </p>
+        ))
+      ) : (
+        <p className="desc">{desc}</p>
+      )}
       {extraTxt && <h4 className="extraTxt">{extraTxt}</h4>}
-
     </div>
-  )
-}
+  );
+};
 
 export default CustomCard;
